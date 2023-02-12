6f6d45e42fbce8d153d564186a252c9aa51ec4d3
Jacob Roth <jakeroth0@gmail.com>
2023-02-11 19:24:00 -0500

    working on a switch statement

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,5 @@
-SELECT role.id AS id, role.title AS title, role.salary AS salary, department.name AS department
-FROM role
-JOIN department ON role.department_id = department.id;
+-- SELECT role.id AS id, role.title AS title, role.salary AS salary, department.name AS department
+-- FROM role
+-- JOIN department ON role.department_id = department.id;
+
+SELECT first_name FROM employee;
