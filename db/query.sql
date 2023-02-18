32377a0476f94601d0bc085de5663422df7c61b6
Jacob Roth <jakeroth0@gmail.com>
2023-02-18 14:05:12 -0500

    all three fof the viewing prompts have been completed and render a table with the correct columns

--- db/query.sql
+++ db/query.sql
@@ -2,4 +2,10 @@
 -- FROM role
 -- JOIN department ON role.department_id = department.id;
 
-SELECT first_name FROM employee;
+-- SELECT first_name FROM employee;
+
+-- SELECT employee.id AS id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ', manager.last_name) AS manager
+-- FROM department
+-- INNER JOIN role ON role.id = department.id
+-- LEFT JOIN employee ON employee.id = role.id
+-- LEFT JOIN employee manager ON employee.manager_id = manager.id;
